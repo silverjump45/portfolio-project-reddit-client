ef08523efadfde6554b58a88fe903e2f0534e5ec
silverjump45 <waltleinen86@gmail.com>
2024-10-24 19:51:01 -0500

    1st draft singlesubreddit and homesubreddit

---
+++ src/features/components/homeSubReddit/homeSubReddit.js
@@ -0,0 +1,14 @@
+import '../singleSubReddit/singleSubReddit.css'
+
+const homeSubReddit = (props) => {
+    console.log(props)
+
+    return (
+        <section className='single-subreddit'>
+            <img src='https://imgs.search.brave.com/r8SaAT0A4evMc0liRJ96rFUPE24EmlAI590hcOjplDI/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/OTFDZFg5dnJ2cDE1/RHIxalMtOWhBSGFI/YSZwaWQ9QXBp'/>
+            <p>{props.name}</p>
+        </section>
+    )
+}
+
+export default homeSubReddit
---
+++ src/features/components/singleSubReddit/singleSubReddit.js
@@ -0,0 +1,40 @@
+import './singleSubReddit.css'
+import { useDispatch } from 'react-redux'
+import { fetchActiveRedditPost, clearPosts } from '../../slices/postsSlice/postsSlice'
+
+const singleSubReddit = (props) => {
+    const dispatch = useDispatch()
+
+    const loadReddit = (e) => {
+        const target = e.targetclosest('section')
+        const allDivs = target.parentElement.children
+        const allChildren = [...allDivs]
+
+        dispatch(clearPosts())
+
+        allChildren.forEach(each => {
+            each.classList.remove('active-reddit')
+        })
+
+        dispatch(fetchActiveRedditPost(props.name))
+        target.classList.add('active-reddit')
+    }
+
+    if(props.name === 'Home') {
+        return (
+            <section className='single-subreddit active-reddit' onClick={loadReddit}>
+                {props.img.includes('http') ? <img src={props.img}/> : <img src='https://imgs.search.brave.com/r8SaAT0A4evMc0liRJ96rFUPE24EmlAI590hcOjplDI/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/OTFDZFg5dnJ2cDE1/RHIxalMtOWhBSGFI/YSZwaWQ9QXBp'/>}
+                <p>{props.name}</p>
+            </section>
+        )
+    }
+
+    return (
+        <section className='single-subreddit' onClick={loadReddit}>
+            {props.img.includes('http') ? <img src={props.img}/> : <img src='https://imgs.search.brave.com/r8SaAT0A4evMc0liRJ96rFUPE24EmlAI590hcOjplDI/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/OTFDZFg5dnJ2cDE1/RHIxalMtOWhBSGFI/YSZwaWQ9QXBp'/>}
+            <p>{props.name}</p>
+        </section>
+    )
+}
+
+export default singleSubReddit
